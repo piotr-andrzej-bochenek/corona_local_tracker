de8bc3e4c2db9b7a6123bba94c8b48c1f8a16bcd
piotr-andrzej-bochenek <piotr8207@interia.eu>
2020-11-10 09:26:30 +0100

    01_Nav add basic navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
+import * as react from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Personal Local SARS-COV-2 Tracker
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to='/about'>
+                About us
+              </Link>
+            </li>
+            <li>
+              <Link to='/maps'>
+                Local maps
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/maps">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+};
+
+function Home() {
+  return <h2>Home</h2>;
 }
 
-export default App;
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
